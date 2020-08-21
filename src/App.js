83218fa8bb9eb587e03e0ff3dc95418e428fd925
wichuon/bussiness-web- <wichuonpra1995@gmail.com>
2020-08-21 15:54:19 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { Route } from 'react-router-dom';
+import './App.css';
+import Header from './components/Header';
+import Home from './components/Home';
+import Blogs from './components/Blogs';
+import About from './components/About';
+import Contact from './components/Contact';
+import BlogDetail from './components/BlogDetail';
+import Slides from './components/Slides';
+
+function App() {
+  return (
+    <div >
+      <Header />
+      <Route path="/" exact={true} component={Home} />
+      <Route path="/about" component={About} />
+      <Route path="/blogs" component={Blogs} />
+      <Route path="/blog/:id" component={BlogDetail} />
+      <Route path="/contact" component={Contact} />
+
+      
+
+    </div>
+  );
+}
+
+export default App;
---
+++ src/data.js
@@ -0,0 +1,9 @@
+export default {
+   videos:[
+       {_id: 1, title:"aaaa", youtube:"", caption:'aaaaaaaaaaaaaaa'},
+       {_id: 2, title:"mmbbbb", youtube:"", caption:'bbbbbbbbbbbb'},
+       {_id: 3, title:"cccccc", youtube:"", caption:'ccccccccccc'}
+       
+   ]
+
+}
